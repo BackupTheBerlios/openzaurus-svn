eb628eeceadc62e988837ffac108abcb98874273
bronson <bronson@67eca316-4f11-0410-8751-bcbd1bec20c1>
2006-06-02 15:50:17 +0000

    Local changes to base MediaWiki 1.5.8.  This is the first working OZ Wiki.

---
+++ wiki/extensions/TimestampNow.php
@@ -0,0 +1,25 @@
+<?php
+
+$wgExtensionFunctions[] = "TimestampNowExtension";
+
+function TimestampNowExtension()
+{
+    global $wgParser;
+
+    # the first parameter is the name of the new tag.
+    # the second parameter is the callback function for
+    # processing the text between the tags
+    $wgParser->setHook("TimestampNow", "RenderTimestampNow");
+}
+
+function renderTimestampNow($input, $argv, $parser=null)
+{
+    # $argv is an array containing any arguments passed to the
+    # extension like <example argument="foo" bar>..
+    if(!$parser) $parser =& $_GLOBALS['wgParser'];
+
+    $parser->disableCache();
+    return wfTimestampNow();
+}
+
+?>
